00e317eac18290af828f17017ac81c090bf46e24
Nicolai Overby Andersen <nico706a@edu.eal.dk>
2019-01-25 20:44:46 +0100

    I changed the views and the they Work

---
+++ Entities/DTO/NewsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTO
+{
+    class NewsDTO
+    {
+        public int NewsId { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string HashTags { get; set; }
+    }
+}
+
