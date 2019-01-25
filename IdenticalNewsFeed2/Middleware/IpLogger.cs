011132f0be5f143d86096badc0e47d4510476d14
Nicolai Overby Andersen <nico706a@edu.eal.dk>
2019-01-25 20:24:58 +0100

    Everything before views work.

---
+++ IdenticalNewsFeed2/Middleware/IpLogger.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdenticalNewsFeed2.Middleware
+{
+    public class IpLogger
+    {
+        private readonly RequestDelegate _next;
+
+        public IpLogger(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvoiceAsync(HttpContext context)
+        {
+            //flesh out.
+
+        }
+    }
+}
